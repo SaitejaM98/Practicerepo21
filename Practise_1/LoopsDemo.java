a7cf4872a2cdffa8cce11a53e62cb4c1cba539a2
Shanumukha sai teja Manikireddy <saitejmanikireddy@gmail.com>
2022-05-17 17:43:00 +0530

    Practise 1A

---
+++ Practise_1/LoopsDemo.java
@@ -0,0 +1,39 @@
+
+public class LoopsDemo {
+
+    private static String[] months =
+            {"January", "February", "March",
+                    "April", "May", "June",
+                    "July", "August", "September",
+                    "October", "November", "December"};
+
+    public static void main(String[] args) {
+        // write your code here
+
+//        for (int i = 0; i < months.length; i++) {
+//            System.out.println(months[i]);
+//        }
+
+//        for (String month : months) {
+//            System.out.println(month);
+//        }
+
+//        int counter = 0;
+//        while (counter < months.length) {
+//            System.out.println(months[counter]);
+//            counter ++;
+//        }
+
+        int counter = 0;
+        do {
+            System.out.println(months[counter]);
+            counter ++;
+
+            if (counter == 9) {
+                break;
+            }
+
+        } while (counter < months.length);
+
+    }
+}
